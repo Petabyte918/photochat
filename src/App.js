e6b24d59d9b2ddc6651a48de0af8a1690842ad04
Egon <a0egon@gmail.com>
2021-08-10 18:26:53 +0200

    first import

--- src/App.js
+++ src/App.js
@@ -1,56 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, {useEffect} from 'react';
 import './App.css';
+import WebCamCapture from "./WebCamCapture";
+import Preview from "./Preview";
+import Chats from "./Chats";
+import ChatView from "./ChatView";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import Login from "./Login";
+import {selectUser} from "./features/appSlice";
+import {auth} from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser) =>{
+      
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Router>
+        {!user ? (<Login />) : (
+          <div className="app__body">
+            <Switch>
+              <Route path="/chats/view"> 
+                <ChatView />
+              </Route>
+              <Route path="/chats"> 
+                <Chats />
+              </Route>
+              <Route path="/preview"> 
+                <Preview />
+              </Route>
+              <Route exact path="/"> 
+                <WebCamCapture />
+              </Route>
+            </Switch>
+          </div>
+        )}
+      </Router>
     </div>
   );
 }
